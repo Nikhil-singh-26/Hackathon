2a42f2a740d40a6ea2a4d1d1b3bac19ae520b228
Ishan6408 <igpatel2006@gmail.com>
2026-02-26 23:58:52 +0530

    fix: resolve TypeError in Mongoose User pre-save hook causing 500 server error on signup

--- backend/models/User.js
+++ backend/models/User.js
@@ -36,11 +36,10 @@ const userSchema = new mongoose.Schema(
 );
 
 // --------------- Pre-save hook: hash password ---------------
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // --------------- Instance method: compare password ---------------
